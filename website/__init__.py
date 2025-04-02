cc698355646cb7a5b96bf7c46a7303eb43c043a7
Sangam Bartaula <114772684+sangambartaula@users.noreply.github.com>
2025-04-02 00:45:26 -0500

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
+import os
 
 def create_app():
-  app = Flask(__name__)
-  app.config['SECRET_KEY'] = 'testWebsite!'
+    app = Flask(__name__, template_folder='website/templates') 
+    app.config['SECRET_KEY'] = 'testWebsite!'
 
-  from .views import views
-  from .auth import auth
+    from .views import views
+    from .auth import auth
 
-  app.register_blueprint(views, url_prefix='/')
-  app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-  return app
+    return app
